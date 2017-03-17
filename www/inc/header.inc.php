71e552b490dcf058a8896fd0c7a8c03e3e14433b
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-17 16:08:23 -0600

    Separated the header and footer
    To avoid redundancy header and footer has been made so all other pages
    can just include them.

---
+++ www/inc/header.inc.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The Big Buy Store</title>
+
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  
+  <script src="script.js"></script>
+  
+</head>
+
+<body>
--- www/index.php
+++ www/index.php
@@ -1,36 +1,28 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>The Big Buy Store</title>
-
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  
-  <script src="script.js"></script>
-  
-</head>
-
-<body>
 <?php
 
-	include ("inc/config.inc.php");
-	
-	echo "Running Diagnostics... <br/>";
-	
-	echo "PHP Installation is ok. Successful php echo().<br/>";
-	
-	$con = mysqli_connect(Conf::DBHOST,Conf::DBUSER,Conf::DBPASS,Conf::DBNAME);
+// Import configuration file
+include ("inc/config.inc.php");
 
-	// Check connection
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	} else {
-		echo "Amazon RDS Connection Established.";
-	}
+// Include header template
+include ("inc/header.inc.php");
 
+
+echo "Running Diagnostics... <br/>";
+
+echo "PHP Installation is ok. Successful php echo().<br/>";
+
+$con = mysqli_connect(Conf::DBHOST,Conf::DBUSER,Conf::DBPASS,Conf::DBNAME);
+
+// Check connection
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+	echo "Amazon RDS Connection Established.";
+}
+
+
+
+// Include footer template
+include ("inc/footer.inc.php");
 	
 ?>
-</body>
-</html>
