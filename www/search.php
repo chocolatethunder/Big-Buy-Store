c597976aae4b87eb579d2d9e3a9adcf003464624
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-23 15:08:22 -0600

    Added search page

---
+++ www/search.php
@@ -0,0 +1,18 @@
+<?php
+// Load classes
+include ("inc/classload.inc.php");
+
+
+// Include header template
+include ("inc/header.inc.php");
+
+?>
+
+
+
+<?php
+
+// Include footer template
+include ("inc/footer.inc.php");
+	
+?>
