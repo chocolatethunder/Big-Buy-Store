e0ab41820e01605d2d4d64e73af9d79243cce7fd
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-23 00:09:11 -0600

    Preload all the classes
    This loads all the classes needed

---
+++ www/inc/classload.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// Import configuration file
+include ("inc/config.inc.php");
+include ("inc/functions.inc.php");
+
+// Import classes
+include ("inc/class_database.inc.php");
+include ("inc/class_registration.inc.php");
+include ("inc/class_formsecurity.inc.php");
+
+$db 			= new pdodb(Conf::DBNAME);
+$securityCheck 	= new security();
+
+?>
