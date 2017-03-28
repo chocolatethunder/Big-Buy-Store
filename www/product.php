d580e23eae18d70aafcf35d1d6c73e5bf32faaca
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-28 01:09:45 -0600

    Added a BLANK Products page

---
+++ www/product.php
@@ -0,0 +1,30 @@
+<?php
+
+// Load classes
+$file = "home";
+include ("inc/classload.inc.php");
+
+
+
+// Include header template
+include ("inc/header.inc.php");
+
+?>
+
+
+<div id = "maincontent">
+	
+	<div id = "productwindow">	
+		
+		
+	
+	</div>
+	
+</div>
+
+<?php
+
+// Include footer template
+include ("inc/footer.inc.php");
+	
+?>
