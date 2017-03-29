9ae148775c2cd16cdaf2906c1f339cda1bca09eb
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-29 00:54:39 -0600

    Feature: shows how many items are in user cart

--- www/inc/header.inc.php
+++ www/inc/header.inc.php
@@ -30,7 +30,7 @@
 				
 					<li class = "market"><a href = "index.php">Marketplace</a></li>
 					<li class = "dash"><a href = "dashboard.php">Dashboard</a></li>
-					<li class = "cart"><a href = "cart.php">Cart</a></li>
+					<li class = "cart"><a href = "cart.php">Cart<?php echo ($user->getNumOfItemsInCart()) ? " (".$user->getNumOfItemsInCart().")" : null ; ?></a></li>
 					<li class = "logout"><a href = "login.php">Logout</a></li>
 				
 				</ul>
