00bd0c260afe4eee9bcdf42a74a019bdce363a44
Tit-tytant <saurabh.tomar@gmail.com>
2017-04-01 21:50:58 -0600

    Added orders list to dashboard

--- www/dashboard.php
+++ www/dashboard.php
@@ -5,6 +5,7 @@ $file = preg_replace('/\.php$/', '', basename(__FILE__));
 include ("inc/classload.inc.php");
 
 // Process
+$orders = $user->getOrders();
 
 // Include header template
 include ("inc/header.inc.php");
@@ -26,7 +27,61 @@ include ("inc/header.inc.php");
 	<br/>
 	
 	<div id = "displaywindow">
-		<div id = "divlabel">View your orders</div>
+	
+	<?php
+			
+			if ($orders) {
+				
+				errorhandler();
+				
+			?>
+				<div id = "divlabel">View your orders</div>
+				
+				<table id="listings">
+						<thead>
+							<tr>
+								<th>Invoice</th>
+								<th>Placed</th>
+								<th>Items</th>
+								<th>Total</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>			
+			
+			<?php
+				
+				foreach ($orders as $order) {
+					
+					echo "<tr>";
+					
+					echo 	"<td id = \"invoice\" data-title=\"Invoice\"><a href = \"addproduct.php?action=edit&id=".$order["oid"]."\">".$order["invoiceid"]."</a></td>";
+					
+					echo 	"<td id = \"orderplaced\" data-title=\"Placed\">".toDate($order["orderDate"])."</td>";
+					
+					echo 	"<td id = \"items\" data-title=\"Items\">".$order["count"]."</td>";
+					echo	"<td id = \"total\" data-title=\"Total\">$".$order["total"]."</td>";
+					echo	"<td id = \"orderstatus\" data-title=\"Status\">".$order["ordstatus"]."</td>";
+
+					echo "</tr>";
+					
+				}
+			
+			?>
+			
+					</tbody>
+				</table>
+			
+			<?php
+				
+			} else {
+				
+				$_SESSION["caution"] = "You have not placed any orders.";
+				errorhandler();
+				
+			}
+			
+			?>
 	</div>
 
 </div>
